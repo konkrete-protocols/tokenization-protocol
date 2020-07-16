93384d4122748510131a26c3700622c274d5cc87
akshay-srivastav <akshay@somish.com>
2020-07-16 11:18:05 +0530

    Tokenization master address saved

--- scripts/common/addresses.js
+++ scripts/common/addresses.js
@@ -1,8 +1,10 @@
 module.exports = {
     kovan: {
         poolToken: "0xf93D64DF3539aED415a9BAb966A637532E99b388",
+        tokenizationMaster: "0xae5A9543299dfa67c2cE604DA0Fbd981625ADE38",
     },
     main: {
         poolToken: "0x0",
+        tokenizationMaster: "0xC829f168462c4Da2C81B43F597E5a5f5635cb3B2",
     },
 };
